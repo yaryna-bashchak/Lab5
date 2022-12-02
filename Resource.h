42e1611eb834c0423d7fbc1781f2aeb9512d93eb
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-12-02 15:24:04 +0200

    add button to show/hide table

--- Resource.h
+++ Resource.h
@@ -32,6 +32,8 @@
 #define IDM_ELLIPSE                     32780
 #define IDM_OLINEO                      32781
 #define IDM_CUBE                        32782
+#define ID_32783                        32783
+#define IDM_TABLE                       32784
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -40,7 +42,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_COMMAND_VALUE         32785
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
